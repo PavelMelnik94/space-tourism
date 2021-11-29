a9032fadd080c05c187e811ef6b5978e6d969321
paveldev <p.melnik@intelico.su>
2021-11-29 14:41:38 +0300

    refactor styles

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { fadeIn, flash } from "react-animations";
+import React from "react";
+import { fadeIn } from "react-animations";
 import styled, { keyframes } from "styled-components";
 
-import "./main.scss"
+import "./main.scss";
 const FadeInAnimation = keyframes`${fadeIn}`;
 const FadeInDiv = styled.div`
   animation: 1s ${FadeInAnimation};
 `;
 
-
-function Main({className,children}) {
-    return (
-        <FadeInDiv id="style-1 wow-effect" className={`main ${className}`} >
-        {children}
-      </FadeInDiv>
-    )
+function Main({ className, children }) {
+  return (
+    <FadeInDiv id="style-1 wow-effect" className={`main ${className}`}>
+      {children}
+    </FadeInDiv>
+  );
 }
 
-export default Main
+export default Main;
--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Main from "../components/Main/Main";
 import data from "./../json/data";
 import "./destination.scss";
